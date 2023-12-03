945c0e432db2ee65dcce28822ef6163725fc4f22
M.Z <zeynalov@gmail.com>
2023-12-03 20:00:16 +0400

    Bad design

--- Common/Services/StringLineReader.cs
+++ Common/Services/StringLineReader.cs
@@ -16,5 +16,18 @@ namespace Common.Services
             }
             return lines; 
         }
+
+        public char[][] ReadArray(string fileName)
+        {
+            var lines = new List<char[]>();
+            using (var fileStream = File.OpenRead(fileName))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true))
+            {
+                String line;
+                while ((line = streamReader.ReadLine()) != null)
+                    lines.Add(line.ToArray());
+            }
+            return lines.ToArray();
+        }
     }
 }
